8ccb5936f23ed9ae60e336bc31036f004c3ae614
Thibaut Patel <thibaut.patel@gmail.com>
2018-12-02 21:47:09 +0100

    Made some progress in the parsing. Added playerElim parsing.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
 const parser = require('./lib/parser.js');
 
-const file = './data/test3.replay'
+const file = './data/UnsavedReplay-2018.07.15-23.26.39.replay';
 
 const buffer = fs.readFileSync(file);
 
 var data = parser.parse(buffer);
 
-console.log(JSON.stringify(data.header, null, 2));
+// Use the data to output the whole parsed object
+console.log(JSON.stringify(data, null, 2));
+
+// Use the data to output the kills
+console.log(
+  data.chunks
+    .filter((c) => c.res && c.res.group == 'playerElim')
+    .map((c) => {
+      c.res.res.time = c.res.time1;
+      return c.res.res;
+    })
+    .map((c) => `${require('./lib/utils').msToInterval(c.time)}  [${c.killer}] eliminated [${c.killed}] [${c.type}]`)
+    .join('\n')
+);
